59f10ed11c7146d7fab06e6da3552c28e120f46d
tim blazina <tim@tims-MacBook-Pro.local>
2016-11-02 15:25:56 +0100

    Added mapStateToProps to MainDash component

--- app/components/MainDash/index.js
+++ app/components/MainDash/index.js
@@ -5,12 +5,11 @@
 */
 
 import React from 'react';
-
-import { FormattedMessage } from 'react-intl';
+import { connect } from 'react-redux';
 import styles from './styles.css';
 import ChartContainer from '../../containers/ChartContainer'
 import {Grid, Row, Col} from 'react-bootstrap';
-
+import {createStructuredSelector} from 'reselect';
 
 
 class MainDash extends React.Component { // eslint-disable-line react/prefer-stateless-function
@@ -19,8 +18,8 @@ class MainDash extends React.Component { // eslint-disable-line react/prefer-sta
       <div className={styles.mainDash}>
          <Grid className={styles.bsGrid} fluid={true}>
    <Row className="show-grid">
-      <Col md={6} mdPush={6}> <ChartContainer url={'http://localhost:8000/d3/location-category-sales'} /> </Col>
-      <Col md={6} mdPull={6}> <ChartContainer url={'http://localhost:8000/d3/weekly-sales'} /> </Col>
+      <Col md={6} mdPush={6}> <ChartContainer url={'http://localhost:8000/d3/location-category-sales'} data={this.props.apidata1}/> </Col>
+      <Col md={6} mdPull={6}> <ChartContainer url={'http://localhost:8000/d3/weekly-sales'} data={this.props.apidata2 }/> </Col>
     </Row>
 
     </Grid>
@@ -28,5 +27,14 @@ class MainDash extends React.Component { // eslint-disable-line react/prefer-sta
     );
   }
 }
+const selectData1 = () => (state) => state.chartContainer;
+const selectData2 = () => (state) => state.chartContainer;
+console.log(selectData1())
 
-export default MainDash;
+const mapStateToProps = createStructuredSelector({
+  apidata1: selectData1(),
+  apidata2: selectData2(),
+});
+
+
+export default connect(mapStateToProps)(MainDash);
